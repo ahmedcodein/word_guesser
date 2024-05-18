9242e20214e569b221bb494ceec60830c5264c64
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-05-18 21:28:46 +0000

    feat: add clear screen method to gamemixins

--- classes/gamemixins.py
+++ classes/gamemixins.py
@@ -1,7 +1,21 @@
+import os
+
+
 class GameMixins:
     """
     This class contains all mixins
     methods that can be used by other
     classes
     """
-    
+
+    def clear_screen(self):
+        """
+        The method clears the game screen
+        """
+        # The following 4 lines of code is taken from:
+        # https://www.delftstack.com/howto/python/python-clear-console/
+        # and https://github.com/dnlbowers/battleships
+        ommand = "clear"
+        if os.name in ("nt", "dos"):  # If Machine is running on Windows, use cls
+            command = "cls"
+            os.system(command)
