c42b71281c9003fcb9811327f1a87ba9e6a36e7a
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-05-19 14:57:05 +0000

    style: format DifficultySelection to remove Lint errs

--- classes/difficultyselection.py
+++ classes/difficultyselection.py
@@ -14,12 +14,16 @@ class DifficultySelection:
     def game_main_loop(self):
         while True:
             self.message = print(
-            "Please enter either 1 or 2 or 3 for the difficulty level:\n\n"
-            "1. Easy\n"
-            "2. Intermediate\n"
-            "3. Difficult\n"
+                "Please enter either 1 or 2 or 3 for the difficulty level:\n\
+                1. Easy\n\
+                2. Intermediate\n\
+                3. Difficult\n"
             )
-            self.difficulty_level = {"easy": 1, "intermediate": 2, "difficult": 3}
+            self.difficulty_level = {
+                "easy": 1,
+                "intermediate": 2,
+                "difficult": 3
+                }
             self.get_difficulty_level()
 
     def get_difficulty_level(self):
@@ -33,13 +37,14 @@ class DifficultySelection:
         while True:
             try:
                 self.difficulty_level_choice = int(
-                    input("Please enter you choice here: " "\n")
+                    input("Please enter you choice here:\n")
                 )
                 loop = True
                 for key, value in self.difficulty_level.items():
-                    if value == self.difficulty_level_choice and loop == True:
+                    if value == self.difficulty_level_choice and loop is True:
                         print(
-                            f"\nyou choose {key.capitalize()} as the difficulty level!\n"
+                            f"You chose '{key.capitalize()}' "
+                            "as the difficulty level!\n"
                         )
                         WordSelector(value, WordBank().words)
                         loop = False
@@ -53,6 +58,6 @@ class DifficultySelection:
                         continue
 
             except ValueError:
-                print("\nInvalid choice, please enter either 1,2 or 3:\n")
+                print("Invalid choice, please enter either 1,2 or 3:\n")
             except KeyboardInterrupt:
                 print("\nCtrl C is not allowed!")
