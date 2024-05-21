86f83b2022703afd234ce6c3a711a19591d310d3
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-05-21 22:53:03 +0000

    style: decrease the length of paramenter names

--- classes/wordselector.py
+++ classes/wordselector.py
@@ -8,10 +8,10 @@ class WordSelector:
      and randomly select a word for the user to guess accordingly
     """
 
-    def __init__(self, name, difficulty_key, difficulty_value, word_bank):
+    def __init__(self, name, dif_key, dif_value, word_bank):
         self.name = name
-        self.difficulty_key = difficulty_key
-        self.difficulty_value = difficulty_value
+        self.dif_key = dif_key
+        self.dif_value = dif_value
         self.word_bank = word_bank
         self.counter = len(word_bank)
         self.difficulty_mapper()
@@ -24,16 +24,16 @@ class WordSelector:
         with specified length
         """
 
-        if self.difficulty_value == 1:
-            self.word_length = 3
-        elif self.difficulty_value == 2:
-            self.word_length = 5
+        if self.dif_value == 1:
+            self.word_len = 3
+        elif self.dif_value == 2:
+            self.word_len = 5
         else:
-            self.word_length = 6
+            self.word_len = 6
         while self.counter > 0:
-            self.word_container = random.choice(self.word_bank)
+            self.word = random.choice(self.word_bank)
             self.counter -= 1
-            if len(self.word_container) == self.word_length:
+            if len(self.word) == self.word_len:
                 break
-        print(f"You have {self.word_length} chances before you lose!\n")
-        PlayerGuess(self.name, self.difficulty_key, self.word_length, list(self.word_container))
+        print(f"You have {self.word_len} chances before you lose!\n")
+        PlayerGuess(self.name, self.dif_key, self.word_len, list(self.word))
