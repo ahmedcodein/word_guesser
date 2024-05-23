d2d9218e0600d80dc08471eda811c0dee1a3c8cc
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-05-23 01:07:33 +0000

    docs: improve the comments language in WordSelector

--- classes/wordselector.py
+++ classes/wordselector.py
@@ -4,8 +4,9 @@ from classes.playerguess import PlayerGuess
 
 class WordSelector:
     """
-    This class takes the difficulty level and the list of words
-     and randomly select a word for the user to guess accordingly
+    This class takes the difficulty level
+    and the words's list and randomly selects
+    a word for the player to guess
     """
 
     def __init__(self, name, dif_key, dif_value, word_bank):
@@ -13,15 +14,15 @@ class WordSelector:
         self.dif_key = dif_key
         self.dif_value = dif_value
         self.word_bank = word_bank
-        self.counter = len(word_bank)
+        self.counter = len(word_bank) # Ensures a complete search
         self.difficulty_mapper()
 
     def difficulty_mapper(self):
         """
         The method maps the difficulty level
-        to the length of word.
-        and then randomly select the word from the list
-        with specified length
+        to the length of the game word. Then
+        it randomly selects the word from the
+        word's bank with specified length
         """
 
         if self.dif_value == 1:
